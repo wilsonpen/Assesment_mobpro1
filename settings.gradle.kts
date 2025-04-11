1009a1e5db7426fb3efc2b58456a148ebee6aac1
Athallah Khansa Ziven Jaya Bahtiar <ziven23junior@gmail.com>
2025-04-11 20:23:58 +0700

    Asesment mobpro1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,6 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
 
 rootProject.name = "Asesment 1"
